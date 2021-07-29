a617ad74813465217b8031ca80a96110a345d8b6
Lucas <lucas.moquet95@gmail.com>
2021-07-29 12:48:40 +0200

    page currency ok

--- pages/[currency].js
+++ pages/[currency].js
@@ -1,18 +1,37 @@
-export default function Currency(res) {
-    console.log(res);
+import Layout from "../components/Layout";
+
+export default function Currency({ res }) {
+
     return (
-        <div>
-            <h1>Currency</h1>
-        </div>
+        <Layout page={"Page " + res.name}>
+            <div className="relative hover:shadow-md p-8 border border-blue-300 sm:rounded-3xl bg-blue-100 md:w-auto flex-1 mx-5">
+                <div className="text-center">
+                    <img
+                        src={res.logo_url}
+                        alt={res.name}
+                        className="w-20 h-20 mx-auto mb-6"
+                    />
+                </div>
+                <h2 className="text-2xl mb-6 uppercase tracking-wider">{res.name}</h2>
+                <p>{res.description}</p>
+                <p className="pt-5 text-blue-500">
+                    <a target="_blank" href={res.reddit_url}>
+                        {res.reddit_url}
+                    </a>
+                </p>
+            </div>
+        </Layout>
     )
 }
 
 export async function getServerSideProps({ query }) {
+    console.log("query", query.currency)
     try {
         const res = await fetch(
-            `https://api.nomics.com/v1/currencies?key=beba8174ea4c8d1ff5c042b4ebdbaa86&ids=${query.currency}&attributes=id,name,logo_url,description`
+            `https://api.nomics.com/v1/currencies?key=beba8174ea4c8d1ff5c042b4ebdbaa86&ids=${query.currency}&attributes=id,name,logo_url,description,reddit_url`
         );
         const result = await res.json();
+        console.log("res", result[0].description)
         return {
             props: { res: result[0] },
         };
